b254859fb9dddcc7f67adfc2f482b224db49189c
macmogu <mm@moguf2k.com>
2024-05-12 21:43:09 +0800

    telescope

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,8 +1,17 @@
 return {
      {
-         "folke/tokyonight.nvim",
-         config = function()
-             vim.cmd[[colorscheme tokyonight-storm]]
-         end
-     },
+        "folke/tokyonight.nvim",
+        dependencies = {
+           "nvim-lualine/lualine.nvim",
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            vim.cmd[[colorscheme tokyonight-storm]]
+            require('lualine').setup({
+                options = {
+                    theme = 'tokyonight'
+                },
+            })
+        end
+    },
 }
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,23 @@
+return {
+    {
+        "akinsho/bufferline.nvim",
+        config = true, 
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        config = true,
+    },
+    {
+        "goolord/alpha-nvim",
+        config = function()
+            require'alpha'.setup(require'alpha.themes.dashboard'.config)
+        end
+    },
+    {
+        "RRethy/vim-illuminate",
+        config = function()
+            require('illuminate').configure()
+        end
+    },
+}
+
